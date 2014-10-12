0d4b0dd7fade05430bac4a78c9c887613d7615a9
Isaac Parker <isaacparker@gmail.com>
2014-10-13 09:18:49 +1000

    Add required Android.mk and remove unneed files
    Change-Id: I50c68b5a16e97731b0ae58501f8457e7207f6a32

---
+++ Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES += $(call all-java-files-under, src)
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4_13 \
+    volley
+
+LOCAL_PACKAGE_NAME := Venture
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_SDK_VERSION := 19
+
+include $(BUILD_PACKAGE)
+
+# Support library v4
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+    android-support-v4_13:/libs/android-support-v4.jar
+
+include $(BUILD_MULTI_PREBUILT)
--- gen/com/venturerom/venture/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.venturerom.venture;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
